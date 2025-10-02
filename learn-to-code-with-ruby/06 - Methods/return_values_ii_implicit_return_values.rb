171284223865495dad991cac1c37fa4cfbcb934c
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-02 18:37:29 -0300

    completed video 69 (lol) - return variables 2: implicit return values

---
+++ learn-to-code-with-ruby/06 - Methods/return_values_ii_implicit_return_values.rb
@@ -0,0 +1,19 @@
+def add_two_numbers(num1, num2)
+  # One branch of logic
+  #   return keyword
+  
+  # Another branch of logic
+  num1 + num2
+end
+
+puts add_two_numbers(3, 4)
+
+def nothing
+  puts "What will the return value be?" # nil
+end
+
+p nothing
+
+#result = nothing
+#p result
+#p result.class
