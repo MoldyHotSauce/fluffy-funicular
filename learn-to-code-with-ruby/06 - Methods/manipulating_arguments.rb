7e8defa8978065c7bfc6b8b2b594914a0155b250
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-02 17:40:48 -0300

    completed video 66 - manipulating arguments

---
+++ learn-to-code-with-ruby/06 - Methods/manipulating_arguments.rb
@@ -0,0 +1,15 @@
+def praise_person(name, age)
+  puts "#{name.upcase} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+  puts "They are #{age + 5} years old"
+end
+
+praise_person("Nate", 34)
+praise_person "Kat", 36
+#praise_person(20, 20)
+
+
+
+#praise_person
+#praise_person("Nate", "Kat")
