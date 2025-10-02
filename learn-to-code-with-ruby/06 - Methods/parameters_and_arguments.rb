56563b0a1d0cc9da94392ed8491819844c3bfd2e
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-02 17:30:49 -0300

    completed video 64 - parameters and arguments

---
+++ learn-to-code-with-ruby/06 - Methods/parameters_and_arguments.rb
@@ -0,0 +1,11 @@
+def praise_person(name)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+end
+
+praise_person("Nate")
+praise_person "Kat"
+
+#praise_person
+#praise_person("Nate", "Kat")
