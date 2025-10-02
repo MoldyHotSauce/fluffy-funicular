8a294a1af96b291571a25deee39cab34adb50dc2
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-02 17:35:11 -0300

    completed video 65 - multiple parameters

---
+++ learn-to-code-with-ruby/06 - Methods/multiple_parameters.rb
@@ -0,0 +1,12 @@
+def praise_person(name, age)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+  puts "They are #{age} years old"
+end
+
+praise_person("Nate", 34)
+praise_person "Kat", 36
+
+#praise_person
+#praise_person("Nate", "Kat")
