e4fd3e76ac60854c751cbc17bc57510cf5238f95
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-03 11:39:02 -0300

    completed video 82 - predicate methods in if statements

---
+++ learn-to-code-with-ruby/08 - Conditionals/predicate_methods_in_if_statements.rb
@@ -0,0 +1,13 @@
+if 5.odd?
+  puts "The number is indeed odd"
+end
+
+word = "kangaroo"
+
+if word.include?("roo")
+  puts "that substring does exist"
+end
+
+if word.include?("zebra")
+  puts "that substring does exist"
+end
