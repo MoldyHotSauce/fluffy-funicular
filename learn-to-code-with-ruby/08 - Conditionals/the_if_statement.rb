b6199942bc5070e7e7308d39b497b3b3f4c3a73c
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-03 11:35:23 -0300

    completed video 81 - the if statement

---
+++ learn-to-code-with-ruby/08 - Conditionals/the_if_statement.rb
@@ -0,0 +1,20 @@
+if 5 < 7
+  puts "That math statement is true"
+end
+
+if 5 < 3
+  puts "Something totally different here"
+end
+
+password = "topsecret"
+
+if password == "topsecret"
+  "Congrats, you logged in!"
+end
+
+word = "kangaroo"
+
+if word.length == 8
+  puts "Hoorat, the word has 8 characters!"
+  puts "Another line"
+end
