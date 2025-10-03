8a882c7ef8ce9868d8d6a6a4272d492ffbc73bd6
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-03 08:59:28 -0300

    completed video 74 - introduction to blocks with the times method

---
+++ learn-to-code-with-ruby/07 - Blocks/introduction_to_blocks_with_the_times_method.rb
@@ -0,0 +1,16 @@
+# Argument - object, concrete value
+# Blocks - a procedure, collection of code
+ 
+8.times { puts "Ruby is awesome!" }
+
+3.times do
+  puts "Ruby is awesome!"
+  puts "I'm having fun learning it!"
+end
+
+value = 3.times do
+  puts "Ruby is awesome!"
+  puts "I'm having fun learning it!"
+end
+
+puts value
