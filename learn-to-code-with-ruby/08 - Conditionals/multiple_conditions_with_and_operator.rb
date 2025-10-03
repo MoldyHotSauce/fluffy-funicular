a3098a17bf2885b32890ec1ac5b1486bec3ecdcf
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-03 13:35:16 -0300

    completed video 88 - multiple conditions with "&&" (and) operator

---
+++ learn-to-code-with-ruby/08 - Conditionals/multiple_conditions_with_and_operator.rb
@@ -0,0 +1,12 @@
+# && == AND
+
+puts "Please enter username"
+username = gets.chomp
+puts "Please enter password"
+password = gets.chomp
+
+if username == "rubydev1" && password == "topsecret"
+  puts "Congrats, you logged in!"
+else
+  puts "Incorrect, no access for you!"
+end
