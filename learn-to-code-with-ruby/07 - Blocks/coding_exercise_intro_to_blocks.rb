e2f94c8dd4dd9e7f9a77db059f513ae72c066934
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-03 09:21:13 -0300

    completed coding exercises - intro to blocks

---
+++ learn-to-code-with-ruby/07 - Blocks/coding_exercise_intro_to_blocks.rb
@@ -0,0 +1,29 @@
+# Define a print_five_times method that accepts no arguments.
+# It should output the text "Hello" 5 times using the times method.
+# Use the print method in your solution.
+#
+# Examples
+#
+# print_five_times() should output "HelloHelloHelloHelloHello"
+
+def print_five_times
+  5.times { print "Hello" }
+end
+
+puts print_five_times
+
+# Define a money_printer method that accepts a single numeric argument.
+# The number specifies how many times the method should print the
+# string "Money" in sequence. Use the 'print' method in your solution.
+#
+# Examples
+#
+# money_printer(3) should output MoneyMoneyMoney
+# money_printer(5) should output MoneyMoneyMoneyMoneyMoney
+# money_printer(0) should output nothing
+
+def money_printer(money = 0)
+  money.times { print "Money"}
+end
+
+puts money_printer(5)
