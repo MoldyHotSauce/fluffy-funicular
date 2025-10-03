a32960b2a7f4b9064c896e88898526e246bc8884
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-03 12:55:56 -0300

    completed video 85 - if elsif

---
+++ learn-to-code-with-ruby/08 - Conditionals/if_and_elsif.rb
@@ -0,0 +1,21 @@
+colour = "Red"
+
+if colour == "Red"
+  puts "Red is meh"
+elsif colour == "Yellow"
+  puts "Yay for yellow"
+elsif colour == "Green"
+  puts "Green is great"
+end
+
+number = 149
+
+if number < 25
+  puts "That is a small number"
+elsif number < 50
+  puts "That is a medium-sized number"
+elsif number < 75
+  puts "That's a big number"
+elsif number < 150
+  puts "That's a humongous number"
+end
