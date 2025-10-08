419300885fcaa81acf9b51c39bf5dac4e2521d4f
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-08 08:57:36 -0300

    Update nested_if_statements.rb

---
+++ learn-to-code-with-ruby/08 - Conditionals/nested_if_statements.rb
@@ -0,0 +1,6 @@
+def meal_plan(time_of_week, time_of_day)
+if time_of_week == "weekday" && time_of_day == "morning"
+  "cereal"
+elsif time_of_week == "weekday" && time_of_day == "night"
+  "Chicken nuggets"
+end
