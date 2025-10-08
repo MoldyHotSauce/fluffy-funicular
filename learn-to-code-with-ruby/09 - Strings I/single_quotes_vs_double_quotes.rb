faadde3807a0fa4d37b623faf7be532805b8440c
Mouldy Hot Sauce <nate@zephyrmanager.io>
2025-10-08 13:59:59 -0300

    completed video 100 - single quotes vs double qoutes

---
+++ learn-to-code-with-ruby/09 - Strings I/single_quotes_vs_double_quotes.rb
@@ -0,0 +1,10 @@
+puts "Hello\n\n\nworld"
+puts 'Hello\n\n\nworld'
+
+# C:\Documents\news
+puts "C:\Documents\news"
+puts 'C:\Documents\news'
+
+phrase = "Hello world"
+puts "My phrase is #{phrase}"
+puts 'My phrase is #{phrase}'
